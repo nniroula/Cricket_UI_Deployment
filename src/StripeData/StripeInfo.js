6c251563172550762411d271528bfc41b873f161
nabin <nkbhaie2017@gmail.com>
2022-11-12 15:39:00 -0700

    set publishable api key

--- src/StripeData/StripeInfo.js
+++ src/StripeData/StripeInfo.js
@@ -22,9 +22,10 @@ import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { CheckoutForm } from "./CheckoutForm";
-import SECRET_API_KEY from "../config";  // secret api key
+// import SECRET_API_KEY from "../config";  // secret api key
 
-const stripeTestPromise = loadStripe(SECRET_API_KEY);
+// const stripeTestPromise = loadStripe(SECRET_API_KEY);
+const stripeTestPromise = loadStripe(process.env.PUBLIC_API_KEY);
 
 const StripeInfo = () => {
   return (
