237b9a7ed45c139c030597ce96d58cf7a0cc2ec2
nabin <nkbhaie2017@gmail.com>
2022-12-22 11:53:10 -0700

    add password validator file

--- cricket-ui/src/validators/PasswordValidator.js
+++ cricket-ui/src/validators/PasswordValidator.js
@@ -1,12 +1,7 @@
 const PasswordValidator = (password) => {
-    // const errors = [];
-
     if(password === undefined || password === ''){
-        // errors.password = 'Password is required.'
-        // errors.push('Password is required');
         return 'Password is required';
     }
-    // return errors;
     return false;
 }
 
