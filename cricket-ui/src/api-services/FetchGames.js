4bd762c8ab65f6652ba5252140fdb4447a5cc76c
nabin <nkbhaie2017@gmail.com>
2023-01-03 07:14:13 -0700

    refactor and clean up the file

--- cricket-ui/src/api-services/FetchGames.js
+++ cricket-ui/src/api-services/FetchGames.js
@@ -1,31 +1,29 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 import DisplayGames from '../components/games/DisplayGames';
-import { useNavigate } from 'react-router-dom';
 import { RETRIEVE_GAMES_URL  } from '../components/Constant';
+import styles from './../stylesheet/Admins.module.css';
 
 
 const FetchGames = () => {
     const [games, setGames] = useState({});
     const [clicked, setClicked] = useState(false);
-    const navigate = useNavigate();
 
     const getAllGames = () => {
-        // const url = 'http://localhost:3000/games';
         const url = RETRIEVE_GAMES_URL ;
-        const gamesData = axios.get(url).then((response) => {
+        axios.get(url).then((response) => {
             const data = response.data;
             setGames(data);
            
         }).catch(error => console.error(error));
- 
-        navigate('/games');
         setClicked(!clicked);
     }
 
     return (
         <div>
-            {clicked ? <DisplayGames games={games} clicked={clicked} /> : <button onClick={getAllGames}>View Games</button> }
+            { clicked ? <DisplayGames games={games} clicked={clicked} /> 
+                : <button className={styles.ViewButtons} onClick={getAllGames}>View Games</button> 
+            }
         </div>
     )
 }
