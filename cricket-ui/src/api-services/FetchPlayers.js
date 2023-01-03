e7763dce265cd67ccb261133e22097b4d07c2c7c
nabin <nkbhaie2017@gmail.com>
2023-01-03 07:17:28 -0700

    refactor the code and clean up the file

--- cricket-ui/src/api-services/FetchPlayers.js
+++ cricket-ui/src/api-services/FetchPlayers.js
@@ -1,30 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import DisplayPlayers from '../components/players/DisplayPlayers';
 import { RETRIEVE_PLAYERS_URL } from '../components/Constant';
-import { useNavigate } from 'react-router-dom';
 import styles from './../stylesheet/Admins.module.css';
 
 const FetchPlayers = () => {
     const [players, setPlayers] = useState({});
     const [clicked, setClicked] = useState(false);
-    const navigate = useNavigate();
 
     const getAllPlayers = () =>{
         const url = RETRIEVE_PLAYERS_URL;
-            axios.get(url).then((response) => {
+        axios.get(url).then((response) => {
             const data = response.data;
             setPlayers(data);
         }).catch(error => console.error(error));
-
         setClicked(!clicked);
-        navigate('/fetchPlayers');
     }
 
     return(
         <div>
-            {clicked ? <DisplayPlayers players={players} clicked={clicked} /> 
-            : <button className={styles.ViewButtons} onClick={getAllPlayers}>View Players</button> }
+            { clicked ? <DisplayPlayers players={players} clicked={clicked} /> 
+                : <button className={styles.ViewButtons} onClick={getAllPlayers}>View Players</button> 
+            }
         </div>
     );
 }
