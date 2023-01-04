c075eded34aad53c9d491987b8a1d92fb86d230d
nabin <nkbhaie2017@gmail.com>
2023-01-04 09:33:40 -0700

    refactor the code

--- cricket-ui/src/components/Home.js
+++ cricket-ui/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './../stylesheet/HomePage.module.css';
-
+ 
 const Home = () => {
     return (
         <div className={styles.backgroundImage}> </div>
