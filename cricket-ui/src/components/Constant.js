fca6a71592fdd179c25ccc973ded813fede2705c
nabin <nkbhaie2017@gmail.com>
2023-01-04 12:24:22 -0700

    refactor the constant file

--- cricket-ui/src/components/Constant.js
+++ cricket-ui/src/components/Constant.js
@@ -4,11 +4,13 @@ const RETRIEVE_ADMINS_URL = 'http://localhost:3000/users/admins';
 const CREATE_ADMIN_ENDPOINT = 'http://localhost:3000/users/admin';
 const PLAYERS_URL = 'http://localhost:3000/players';
 const SIGN_UP_ENDPOINT = 'http://localhost:3000/users';
+const LOGIN_ENDPOINT = 'http://localhost:3000/user/login';
 
 export {BCRYPT_WORK_FACTOR, 
     RETRIEVE_ADMINS_URL, 
     SIGN_UP_ENDPOINT,
     GAMES_ENDPOINT,
     CREATE_ADMIN_ENDPOINT,
-    PLAYERS_URL
+    PLAYERS_URL,
+    LOGIN_ENDPOINT
 };
