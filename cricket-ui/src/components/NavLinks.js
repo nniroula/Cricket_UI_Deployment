4f5f9b2df799b8c10c03e7d19a172b22be781a31
nabin <nkbhaie2017@gmail.com>
2023-01-04 12:25:50 -0700

    refactor the code in navlinks file

--- cricket-ui/src/components/NavLinks.js
+++ cricket-ui/src/components/NavLinks.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../stylesheet/NavLinks.module.css';
 import { NavLink } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -7,40 +7,40 @@ import logInTracker from './auth/loginTracker';
 
 const NavLinks = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
-    let isSignedIn = false;
-    // const [isSignedIn, setIsSignedIn] = useState(false);
-    const signedInUser = logInTracker(); 
+    const [user, setUser] = useState({});
 
     const buttonToggle = () => {
         setNavbarOpen(!navbarOpen)
     }
 
-    if(signedInUser){
-        isSignedIn = true;
-        // setIsSignedIn(true)
-    }
+    useEffect(() => {
+        setInterval(() => {
+            const signedInUser = logInTracker();
+            setUser(signedInUser);
+            }, [])
+    }, 5000);
 
     return (
         <div className={styles.Navlinks}>
-             {isSignedIn ? <span className={styles.Authentication}><NavLink className={styles.SigningService} to="/logout">Logout</NavLink> </span>
+             {user ? 
+                <span className={styles.Authentication}><NavLink className={styles.SigningService} to="/logout">Logout</NavLink> </span>
                 :  <span className={styles.Authentication}>
                     <span className={styles.SignUp}> <NavLink className={styles.SigningService} to="/signup">Sign Up</NavLink> </span>
                     <span> <NavLink className={styles.SigningService} to="/login">Log In</NavLink> </span>
                 </span>
              }
-            <button className={styles.Menu} onClick={buttonToggle}> {navbarOpen ? 
+            <div className={styles.Menu} onClick={buttonToggle}> {navbarOpen ? 
                 <div className={styles.MenuContent}>
                     <MenuIcon />
-                    <div><NavLink className={styles.MenuItems} to="/admins">Admins</NavLink> </div>
-                    <div><NavLink className={styles.MenuItems} to="/games">Games</NavLink> </div>
-                    <div><NavLink className={styles.MenuItems} to="/players">Players</NavLink> </div>
-                    <div><NavLink className={styles.MenuItems} to="/aboutUs"> About Us </NavLink></div>
+                    <div><NavLink className={styles.MenuItems} to="/fetchGames">Games</NavLink> </div>
+                    <div><NavLink className={styles.MenuItems} to="/fetchPlayers">Players</NavLink> </div>
+                     <div><NavLink className={styles.MenuItems} to="/fetchAdmins">Admins</NavLink> </div>
+                     <div><NavLink className={styles.MenuItems} to="/aboutUs"> About Us </NavLink></div>
                     <div><NavLink className={styles.MenuItems} to="/contactUs"> Contact Us </NavLink></div>
-                    <div><NavLink className={styles.MenuItems} to="/donation"> Donate </NavLink></div>
+                    <div><NavLink className={styles.MenuItems} to="/donation"> Donate </NavLink></div>  
                 </div>
                 : <MenuIcon />}
-
-            </button>
+            </div>
         </div>
     );
 }
