d49196ea8ad5e6588c96d756a5d01eca10cd4fdd
nabin <nkbhaie2017@gmail.com>
2023-01-04 17:46:52 -0700

    add aecc-api base url for production

--- cricket-ui/src/components/Constant.js
+++ cricket-ui/src/components/Constant.js
@@ -1,10 +1,22 @@
 const BCRYPT_WORK_FACTOR = 12;
-const GAMES_ENDPOINT = 'http://localhost:3000/games';
-const RETRIEVE_ADMINS_URL = 'http://localhost:3000/users/admins';
-const CREATE_ADMIN_ENDPOINT = 'http://localhost:3000/users/admin';
-const PLAYERS_URL = 'http://localhost:3000/players';
-const SIGN_UP_ENDPOINT = 'http://localhost:3000/users';
-const LOGIN_ENDPOINT = 'http://localhost:3000/user/login';
+const BASE_URL = 'https://aecc-api.herokuapp.com'
+// const GAMES_ENDPOINT = 'http://localhost:3000/games';
+const GAMES_ENDPOINT = `${BASE_URL}/games`;
+
+// const RETRIEVE_ADMINS_URL = 'http://localhost:3000/users/admins';
+const RETRIEVE_ADMINS_URL = `${BASE_URL}/users/admins`;
+
+// const CREATE_ADMIN_ENDPOINT = 'http://localhost:3000/users/admin';
+const CREATE_ADMIN_ENDPOINT = `${BASE_URL}/users/admin`;
+
+// const PLAYERS_URL = 'http://localhost:3000/players';
+const PLAYERS_URL = `${BASE_URL}/players`;
+
+// const SIGN_UP_ENDPOINT = 'http://localhost:3000/users';
+const SIGN_UP_ENDPOINT = `${BASE_URL}/users`;
+
+// const LOGIN_ENDPOINT = 'http://localhost:3000/user/login';
+const LOGIN_ENDPOINT = `${BASE_URL}/user/login`;
 
 export {BCRYPT_WORK_FACTOR, 
     RETRIEVE_ADMINS_URL, 
